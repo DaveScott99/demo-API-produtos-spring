647f7a6390976a9e873db7d9a6edfe185a97e74d
DaviSantos <davicalixto2077@gmail.com>
2022-09-13 12:54:27 -0300

    initial commit

---
+++ src/main/java/com/astro/myFirstProject/entities/Product.java
@@ -0,0 +1,67 @@
+package com.astro.myFirstProject.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Product implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private Double price;
+	
+	private Category category;
+	
+	public Product() {
+	}
+
+	public Product(Long id, String name, Double price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
